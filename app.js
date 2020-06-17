79fc206e9ef1b54489327de4cb734247e999209a
Paawan Kohli <paawan.kohli10@gmail.com>
2020-06-17 16:20:56 +0530

    minor fix and file renaming

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const getTweetsAsync = require("./util/getTweets.js")
 app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }))
 
 app.get('/getTweet', async (req, res) => {    
-    const data = await getTweetsAsync(req.query.query, req.query.count)
+
+    const query = req.query.query
+    const count = req.query.count
+
+    const data = await getTweetsAsync(query, count)
     const csvData = await converter.json2csvAsync(data)
 
-    res.setHeader('Content-disposition', 'attachment; filename=tweets.csv');
+    res.setHeader('Content-disposition', `attachment; filename=tweets_${query}_${count}.csv`);
     res.set('Content-Type', 'text/csv');
     res.status(200).send(csvData)
 })
